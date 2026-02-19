bc9a3a456f1bc3475a9e737e2d43fcab83b115a5
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2026-02-19 16:53:19 -0300

    docs: Update task status and minor fixes

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useAuth } from './AuthProvider'
 import { useNavigate } from 'react-router-dom'
 import styles from './LoginPage.module.css'
